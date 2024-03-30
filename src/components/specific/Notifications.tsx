265af0a05560d15f6f9aad6b896e6f85578158c4
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-03-30 10:59:11 +0000

    Add Notifications component and update package.json dependencies

---
+++ src/components/specific/Notifications.tsx
@@ -0,0 +1,125 @@
+import React, { memo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Avatar,
+  Button,
+  Dialog,
+  DialogTitle,
+  ListItem,
+  Skeleton,
+  Stack,
+  Typography,
+} from "@mui/material";
+import {
+  useAcceptFriendRequestMutation,
+  useGetNotificationsQuery,
+} from "../../redux/api/api";
+import { setIsNotification } from "../../redux/reducers/misc";
+import { useAsyncMutation, useErrors } from "../../hooks/hook";
+
+interface Sender {
+  name: string;
+  avatar: string; // Assuming avatar is a URL
+}
+
+interface Notification {
+  sender: Sender;
+  _id: string;
+}
+
+const Notifications: React.FC = () => {
+  const { isNotification } = useSelector((state: any) => state.misc);
+
+  const dispatch = useDispatch();
+
+  const { isLoading, data, error, isError } = useGetNotificationsQuery();
+
+  const [acceptRequest] = useAsyncMutation(useAcceptFriendRequestMutation);
+
+  const friendRequestHandler = async ({ _id, accept }: { _id: string; accept: boolean }) => {
+    dispatch(setIsNotification(false));
+    await acceptRequest("Accepting...", { requestId: _id, accept });
+  };
+
+  const closeHandler = () => dispatch(setIsNotification(false));
+
+  useErrors([{ error, isError }]);
+
+  return (
+    <Dialog open={isNotification} onClose={closeHandler}>
+      <Stack p={{ xs: "1rem", sm: "2rem" }} maxWidth={"25rem"}>
+        <DialogTitle>Notifications</DialogTitle>
+
+        {isLoading ? (
+          <Skeleton />
+        ) : (
+          <>
+            {data?.allRequests.length > 0 ? (
+              data?.allRequests?.map(({ sender, _id }: Notification) => (
+                <NotificationItem
+                  sender={sender}
+                  _id={_id}
+                  handler={friendRequestHandler}
+                  key={_id}
+                />
+              ))
+            ) : (
+              <Typography textAlign={"center"}>0 notifications</Typography>
+            )}
+          </>
+        )}
+      </Stack>
+    </Dialog>
+  );
+};
+
+interface NotificationItemProps {
+  sender: Sender;
+  _id: string;
+  handler: ({ _id, accept }: { _id: string; accept: boolean }) => void;
+}
+
+const NotificationItem: React.FC<NotificationItemProps> = memo(({ sender, _id, handler }) => {
+  const { name, avatar } = sender;
+  return (
+    <ListItem>
+      <Stack
+        direction={"row"}
+        alignItems={"center"}
+        spacing={"1rem"}
+        width={"100%"}
+      >
+        <Avatar src={avatar} />
+
+        <Typography
+          variant="body1"
+          sx={{
+            flexGlow: 1,
+            display: "-webkit-box",
+            WebkitLineClamp: 1,
+            WebkitBoxOrient: "vertical",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            width: "100%",
+          }}
+        >
+          {`${name} sent you a friend request.`}
+        </Typography>
+
+        <Stack
+          direction={{
+            xs: "column",
+            sm: "row",
+          }}
+        >
+          <Button onClick={() => handler({ _id, accept: true })}>Accept</Button>
+          <Button color="error" onClick={() => handler({ _id, accept: false })}>
+            Reject
+          </Button>
+        </Stack>
+      </Stack>
+    </ListItem>
+  );
+});
+
+export default Notifications;
